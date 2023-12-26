3770cd03c270f41b46c9645e84b0d54bb53ad1e3
Brenten Dickerson <greatbritain249@gmail.com>
2023-12-25 23:48:38 -0500

    Added Header Banner

---
+++ src/components/HeaderBanner.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './../App.css'; // Import your CSS styles
+interface HeaderBannerProps {
+    bannerImage: string; // SVG media link source
+    bannerLogo: string;
+    bannerText: string;
+    buttonText: string;
+}
+
+const HeaderBanner: React.FC<HeaderBannerProps> = ({ bannerImage, bannerLogo , bannerText, buttonText }) => {
+  return (
+    <div className="header-banner-container">
+            <div className="header-banner-image">
+                <img src={bannerLogo} alt="Banner Logo" className="logo" />
+            </div>
+        <div className="sub-banner">{bannerText}</div>
+        <a href="header-banner-button" className="header-banner-button">{buttonText}</a>
+    </div>
+  );
+};
+
+export default HeaderBanner;
