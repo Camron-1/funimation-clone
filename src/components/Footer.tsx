39643fe6dc33519d21ec403bc871e46c82bb1257
Camron Criddell <criddellcamron1@gmail.com>
2023-12-27 00:46:20 -0500

    Updated footer fixed check box logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,95 +8,117 @@ import youtubeImage from "./../assets/youtube.svg"
 import pinterestImage from "./../assets/pinterest.svg"
 import tiktokImage from "./../assets/tiktok.svg"
 import sonyPicture from "./../assets/2b6e796d-2275-41a6-8b90-5d2b3f89f680.png"
-
+import checkBox from "./../assets/checkmark.png"
 const Footer: React.FC = () => {
   return (
     <footer className="footer-container">
       <div className="footer-links">
         <div className="footer-section">
+          
           <a href="https://www.funimation.com/about-us/" target="https://www.funimation.com/about-us/" rel="noopener noreferrer">
             <h2>ABOUT FUNIMATION</h2>
           </a>
-        </div>
+          <div className="footer-section">
+            <a href="https://help.funimation.com/hc/en-us" target="https://help.funimation.com/hc/en-us" rel="noopener noreferrer">
+              <h2>CUSTOMER SUPPORT</h2>
+            </a>
+          </div>
 
-        <div className="footer-section">
-          <h2>CUSTOMER SUPPORT</h2>
-        </div>
+          <div className="footer-section">
+            <a href="https://www.funimation.com/terms-of-use/" target="https://www.funimation.com/terms-of-use/" rel="noopener noreferrer">
+              <h2>TERMS OF USE</h2>
+            </a>
+          </div>
 
-        <div className="footer-section">
-          <h2>TERMS OF USE</h2>
-        </div>
+          <div className="footer-section">
+            <a href="https://www.funimation.com/terms-of-sale/" target="https://www.funimation.com/terms-of-sale/" rel="noopener noreferrer">
+              <h2>TERMS OF SALE</h2>
+            </a>
+          </div>
 
-        <div className="footer-section">
-          <h2>TERMS OF SALE</h2>
-        </div>
+          <div className="footer-section">
+            <a href="https://www.sonypictures.com/corp/privacy.html" target="https://www.sonypictures.com/corp/privacy.html" rel="noopener noreferrer">
+              <h2>PRIVACY POLICY</h2>
+            </a>
+          </div>
 
-        <div className="footer-section">
-          <h2>PRIVACY POLICY</h2>
-        </div>
+          <div className="footer-section">
+            
+            <a href="https://privacyportal-cdn.onetrust.com/dsarwebform/d19e506f-1a64-463d-94e4-914dd635817d/b9eb997c-9ede-451b-8fd4-29891782a928.html" target="https://privacyportal-cdn.onetrust.com/dsarwebform/d19e506f-1a64-463d-94e4-914dd635817d/b9eb997c-9ede-451b-8fd4-29891782a928.html" rel="noopener noreferrer" >
+              <h2>YOUR PRIVACY CHOICES</h2>
+              <div className="picture-image">
+              <img 
+                src={checkBox} 
+                alt="Check Box"
+                />
+                </div>
+            </a>
+          </div>
 
-        <div className="footer-section">
-          <h2>YOUR PRIVACY CHOICES</h2>
+          <div className="footer-section">
+            <a href="https://www.crunchyroll.com/about/jobs/index.html" target="https://www.crunchyroll.com/about/jobs/" rel="noopener noreferrer">
+              <h2>CAREERS</h2>
+            </a>
+          </div>
+          <div className="footer-section">
+            <img
+              src={sonyPicture}
+              alt="Sony Picture"
+            />
+          </div>
         </div>
+        <div>
+          <div style={{ position: 'absolute', top: 990, right: 90, maxHeight: '1000px', overflow: 'auto' }}>
+            <a href="https://www.facebook.com/funimation" target="https://www.facebook.com/funimation" rel="noopener noreferrer">
+              <img
+                src={facebookImage}
+                alt="Instagram Logo Facebook Logo"
+                style={{ width: '20px', height: '50px', marginRight: '10px' }}
+              />
+            </a>
+            <a href="https://twitter.com/funimation" target="https://twitter.com/funimation" rel="noopener noreferrer">
+              <img
+                src={twitterImage}
+                alt="Twitter Logo"
+                style={{ width: '20px', height: '50px', marginRight: '10px' }}
+              />
+            </a>
+            <a href="https://www.youtube.com/user/FUNimation" target="https://www.youtube.com/user/FUNimation" rel="noopener noreferrer">
+              <img
+                src={youtubeImage}
+                alt="Youtube Logo"
+                style={{ width: '20px', height: '50px', marginRight: '10px' }}
+              />
+            </a>
+            <a href="https://www.instagram.com/funimation" target="https://www.instagram.com/funimation" rel="noopener noreferrer">
+              <img
+                src={instagramImage}
+                alt="Instagram Logo"
+                style={{ width: '20px', height: '50px', marginRight: '10px' }}
+              />
+            </a>
+            <a href="https://www.pinterest.com/funimation/" target="https://www.pinterest.com/funimation/nk" rel="noopener noreferrer">
+              <img
+                src={pinterestImage}
+                alt="Pinterest Logo"
+                style={{ width: '20px', height: '50px', marginRight: '10px' }}
+              />
 
-        <div className="footer-section">
-          <h2>CAREERS</h2>
-        </div>
-        <div className="footer-section">
-          <img
-            src={sonyPicture}
-            alt="Sony Picture"
-          />
+            </a>
+            <a href="https://www.tiktok.com/@funimation" target="https://www.tiktok.com/@funimation" rel="noopener noreferrer">
+              <img
+                src={tiktokImage}
+                alt="Tiktok Logo"
+                style={{ width: '18px', height: '50px', marginRight: '10px' }}
+              />
+            </a>
+          </div>
+          <div className="crunchy-roll-title" style={{ position: 'relative', top: '-100px' }}>
+            <p style={{ fontSize: '12.5px' }}>© Crunchyroll, LLC. All Rights Reserved</p>
+          </div>
+
         </div>
       </div>
-      <div>
-        <div style={{ position: 'absolute', top: 1550, right: 0, maxHeight: '1000px', overflow: 'auto' }}>
-          <a href="https://www.instagram.com/funimation" target="https://www.instagram.com/funimation" rel="noopener noreferrer">
-            <img
-              src={instagramImage}
-              alt="Instagram Logo"
-              style={{ width: '20px', height: '50px', marginRight: '10px' }}
-            />
-          </a>
-          <a href="https://www.facebook.com/funimation" target="https://www.facebook.com/funimation" rel="noopener noreferrer">
-            <img
-              src={facebookImage}
-              alt="Facebook Logo"
-              style={{ width: '20px', height: '50px', marginRight: '10px' }}
-            />
-          </a>
-          <a href="https://twitter.com/funimation" target="https://twitter.com/funimation" rel="noopener noreferrer">
-            <img
-              src={twitterImage}
-              alt="Twitter Logo"
-              style={{ width: '20px', height: '50px', marginRight: '10px' }}
-            />
-          </a>
-          <a href="https://www.youtube.com/user/FUNimation" target="https://www.youtube.com/user/FUNimation" rel="noopener noreferrer">
-            <img
-              src={youtubeImage}
-              alt="Youtube Logo"
-              style={{ width: '20px', height: '50px', marginRight: '10px' }}
-            />
-          </a>
-          <a href="https://www.pinterest.com/funimation/" target="https://www.pinterest.com/funimation/nk" rel="noopener noreferrer">
-            <img
-              src={pinterestImage}
-              alt="Pinterest Logo"
-              style={{ width: '20px', height: '50px', marginRight: '10px' }}
-            />
-
-          </a>
-          <a href="https://www.tiktok.com/@funimation" target="https://www.tiktok.com/@funimation" rel="noopener noreferrer">
-            <img
-              src={tiktokImage}
-              alt="Tiktok Logo"
-              style={{ width: '20px', height: '50px', marginRight: '10px' }}
-            />
-          </a>
-        </div>
-      </div>
-
     </footer>
   );
 };
